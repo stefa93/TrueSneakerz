92240720209f2d3bee04ea3f03a99e8fd5370f99
stefariolowell <srlowell@live.nl>
2018-06-19 01:52:08 +0200

    under construction finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <UnderConstruction></UnderConstruction>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import UnderConstruction from './components/underConstruction/UnderConstruction.vue';
 
 @Component({
   components: {
-    HelloWorld,
+      UnderConstruction,
   },
 })
 export default class App extends Vue {}
@@ -19,11 +18,5 @@ export default class App extends Vue {}
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/underConstruction/UnderConstruction.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-fluid wrapper">
+            <div class="row upper-text"><span class="crane">🏗️</span>TRUESNEAKER</div>
+            <div class="row sub-text"> UNDER CONSTRUCTION</div>
+            <div class="icons">
+                <i class="fab fa-snapchat-ghost fa-2x"></i>
+                <i class="fab fa-instagram fa-2x"></i>
+                <i class="fab fa-youtube fa-2x"></i>
+            </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class UnderConstruction extends Vue {
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped src="./style.scss"></style>
